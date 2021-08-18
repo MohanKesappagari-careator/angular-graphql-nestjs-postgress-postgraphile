4245e98c86126fc097a858ecda28296cab75e273
mohankesappagari <mohan.k@careator.com>
2021-08-18 16:10:37 +0530

    postgraphile

--- nestjs-postgraphile/src/main.ts
+++ nestjs-postgraphile/src/main.ts
@@ -1,12 +1,13 @@
-import { postgraph } from './postgraphile';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { postgraph } from "./postgraphile";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 const { PORT, PASSWORD } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   await app.use(postgraph);
-  
+
   app.listen(PORT, () =>
     console.log(`Server running${PASSWORD} on port ${PORT}`),
   );
